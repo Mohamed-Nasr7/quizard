48f0f5ec502bfeb3504677aeb43e01f89253c127
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-07 22:11:15 +0200

    Set a padding for the .app css class

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <h1 className='title text-center'>Quizard</h1>
       <Home />
     </div>
