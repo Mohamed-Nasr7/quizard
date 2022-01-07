6de9cbdf44cc6b7236d61ce3c5a5c51140bb389d
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-07 16:08:07 +0200

    Wrap the App component around the Provider and pass the store as a prop to the Provider

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
