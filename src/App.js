2897aa3f2228f4be4dd4db0c6af80568b0678f1d
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-07 21:58:12 +0200

    Move the h1 element from Home to App component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className='App'>
+      <h1 className='title text-center'>Quizard</h1>
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Home() {
   return (
     <div className='home pt-5'>
-      <h1 className='home__title text-center'>Quizard</h1>
       <div className='home__quizzes'></div>
     </div>
   );
