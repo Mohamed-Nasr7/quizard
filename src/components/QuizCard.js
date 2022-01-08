028d06f957a50caec27813363ac36f862581301b
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-01-08 23:13:55 +0200

    Add a bootstrap card template to QuizCard.js

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 function quizCard() {
-  return <div></div>;
+  return (
+    <Card border='dark' style={{ width: '18rem' }}>
+      <Card.Header>
+        <Card.Title>Dark Card Title</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default quizCard;
